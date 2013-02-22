2260ddb2f6b0712f895f179be89aa89220987c20
Randy Hunt <letsgetrandy@gmail.com>
2013-02-21 21:38:30 -0600

    staticfiles

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py runserver 0.0.0.0:$PORT --noreload
+web: python my_django_app/manage.py collectstatic --noinput; python manage.py runserver 0.0.0.0:$PORT --noreload
--- mns/urls.py
+++ mns/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+#if not settings.DEBUG:
+#    urlpatterns += patterns('',
+#        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+#    )
