12932fa8e992a33410b01b7c9afc5d2f3b38a993
Randy Hunt <letsgetrandy@gmail.com>
2013-02-21 21:30:24 -0600

    static urls

--- mns/settings.py
+++ mns/settings.py
@@ -2,6 +2,7 @@
 import os
 import dj_database_url
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
 TEMPLATE_DEBUG = DEBUG
 
@@ -60,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
