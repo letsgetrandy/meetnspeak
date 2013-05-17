7a95e2773098392e025e9093060f3d6b799e8df4
Randy "Big Game" Hunt <letsgetrandy@gmail.com>
2013-05-17 04:01:31 -0500

    python 2.7 style logging stream

--- mns/settings.py
+++ mns/settings.py
@@ -202,7 +202,7 @@ LOGGING = {
         'console': {
             'level': 'INFO',
             'class': 'logging.StreamHandler',
-            'strm': 'sys.stdout',
+            'stream': 'sys.stdout',
         },
     },
     'loggers': {
